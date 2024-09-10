f2d756ff68384bb4c4413f6a7f4db6b49d33ecee
Arcega <custodiojohn34@gmail.com>
2024-09-10 22:01:43 +0800

    subtraction method

--- calculator.java
+++ calculator.java
@@ -3,10 +3,13 @@ public class Calculator {
     public static int addition(int a, int b) {
         return a + b;
     }
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
 
     public static void main(String[] args) {
         // Example usage of the calculator methods
         System.out.println("Addition: " + addition(5, 3));
-       
+        System.out.println("Subtraction: " + subtraction(5, 3));
     }
 }
